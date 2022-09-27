d855b7fe770f6252b63193cb03a8c213232b69ec
iskandar <isikjandev@gmail.com>
2022-09-27 21:52:08 +0500

    mobile

--- js/script.js
+++ js/script.js
@@ -8,9 +8,49 @@ $('.banner__content').slick({
     autoplaySpeed: 2000,
     arrows: false
 });
+$('.action__cards').slick({
+    slidesToShow: 3,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [{
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+})
 $('.xit__cards').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows: false
+    arrows: false,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 580,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
